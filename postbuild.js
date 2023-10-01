0fb94684479f21f4d1f995a2ffdb6f282e026c79
Takacs Hunor <tkcshnr@gmail.com>
2023-10-01 18:17:15 +0200

    postbuild

--- postbuild.js
+++ postbuild.js
@@ -9,7 +9,8 @@ readFile(file, 'utf8', function (err, data) {
   let result = 
     data.replace(new RegExp("three/examples/jsm/loaders/DRACOLoader", 'g'), "three/examples/jsm/loaders/DRACOLoader.js")
     .replace(new RegExp("three/examples/jsm/loaders/KTX2Loader", 'g'), "three/examples/jsm/loaders/KTX2Loader.js")
-    .replace(new RegExp("three/examples/jsm/loaders/GLTFLoader", 'g'), "three/examples/jsm/loaders/GLTFLoader.js");
+    .replace(new RegExp("three/examples/jsm/loaders/GLTFLoader", 'g'), "three/examples/jsm/loaders/GLTFLoader.js")
+    .replace(new RegExp("three/examples/jsm/controls/OrbitControls", 'g'), "three/examples/jsm/controls/OrbitControls.js");
 
   writeFile(file, result, 'utf8', function (err) {
     if (err) return console.log(err);
